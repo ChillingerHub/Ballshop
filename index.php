125b6f43f9213e92ad9f1c80c4443aa76ba0f264
Stefan Hillinger <4211@htl.rennweg.at>
2018-01-21 12:42:12 +0100

    Added bootstrap, added balldemo.html

--- index.php
+++ index.php
@@ -12,15 +12,32 @@ use stefan\{
     ball, ball\fussball, ball\basketball, ball\golfball, ball\BallListing
 };
 
-$ball = new golfball("Wilshon", 3, "Weed");
-$ball2 = new golfball("Wilshon", 3, "Weeds");
+$ball_1_1 = new fussball("Stevens", 20, "Leder");
+$ball_1_2 = new fussball("Marksch", 20.3, "Plastik");
 
+$ball_2_1 = new basketball("Wilshon", 31.5, "Leder");
+$ball_2_2 = new basketball("Wilshon", 33.5, "Plastik");
+
+$ball_3_1 = new golfball("Klips", 4, "Plastik");
+$ball_3_2 = new golfball("Knaber", 3.5, "Plastik");
+
+
+
+$ballarray = array(
+    $ball_1_1,
+    $ball_1_2,
+    $ball_2_1,
+    $ball_2_2,
+    $ball_3_1,
+    $ball_3_2
+);
+
+$list = new BallListing($ballarray);
+$list->list();
+
+/*
 echo $ball;
-
-$ballArray = [$ball, $ball2];
-
-$list = new BallListing($ballArray);
-
 echo $list->listHtml();
 echo $list->listJson();
 echo $list->list();
+*/
